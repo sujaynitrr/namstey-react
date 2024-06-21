94af7cdea7ad52143ee8e0185d995e0e10fca489
SujayKumar <sujay.nitrr@gmail.com>
2024-06-21 14:29:02 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,37 @@
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>I am h1 tag</h1>
+     <h1>I am h2 tag</h1>
+  </div>
+  <div id="child2">
+    <h1>I am h1 tag</h1>
+     <h1>I am h2 tag</h1>
+  </div>
+  
+  <div id="parent">
+  <div id="child">
+    <h1>I am h1 tag</h1>
+     <h1>I am h2 tag</h1>
+  </div>
+  <div id="child2">
+    <h1>I am h1 tag</h1>
+     <h1>I am h2 tag</h1>
+  </div>
+  
+</div>; */
+}
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+console.log(heading, "heading");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
