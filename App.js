4671adeb418c5497e3ba535bc5609813b651ec7e
SujayKumar <sujay.nitrr@gmail.com>
2024-06-22 15:57:52 +0530

    Implemeation of init npm parcel git ignore

--- App.js
+++ App.js
@@ -21,6 +21,8 @@
   
 </div>; */
 }
+import React from "react";
+import ReactDOM from "react-dom";
 
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
