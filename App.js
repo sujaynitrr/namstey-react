de003d61f14bb5516e130d3253ca0a5d5cfbe6fa
SujayKumar <sujay.nitrr@gmail.com>
2024-06-22 16:55:48 +0530

    Implementation of Component and react element

--- App.js
+++ App.js
@@ -24,16 +24,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-console.log(heading, "heading");
+//jsx (transpiled before it reaches the js) - parcel - Babel
+
+//jsx => Babel transpile it  it to React.createElement => ReactElement-js Object =>HTMLElement(render)
+
+//React element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    React using jsx
+  </h1>
+);
+
+//React component
+//Class based component - Old
+//Functional component - New
+
+// Functional Component
+
+const Title = () => {
+  return <h1>React using Jsx</h1>;
+};
+
+// Functional Component
+const Footer = () => <h1>React is using jsx</h1>;
+
+//Component inside the component known as composition component
+// below one is example of composition component
+// Calling  Title component as method in side of HeadingComponent
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {Title()}
+      <Title />
+      <h1>Welcome to React component</h1>
+    </div>
+  );
+};
+
+// Using Functional component in side reactElement
+const reactElement = (
+  <h1>
+    <HeadingComponent />
+    Hello React
+  </h1>
+);
+
+const jsxHeading = <h1 id="child">Hello React</h1>;
+console.log(jsxHeading, "jsxHeading");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+// How can Babel identify the component ==> we have to pass  like (</>) inside it pass the component name like  ==><HeadingComponent/>
+
+//root.render(<HeadingComponent />);
+
+root.render(reactElement);
